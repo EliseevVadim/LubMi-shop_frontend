316307e76305068994cbe9945c3352b8a667fdc5
rmelinevskiy <150797126+Arkadaweb@users.noreply.github.com>
2024-08-02 12:54:20 +0300

    Revert "fix"
    This reverts commit 9e55d996aec37d01ee4a5fd522ae51f4b16e9605.

--- src/entety/modals/UI/CheckoutModal.tsx
+++ src/entety/modals/UI/CheckoutModal.tsx
@@ -135,11 +135,6 @@ const CheckoutModal = () => {
     }
   }, [selectedCities, selectedStreet, selectedBuilding])
 
-  useEffect(() => {
-    if (!isRussianPostAvaible && selectedDelivery === 'pr'){
-      onSelectDelivery('cd')
-    }
-  }, [isRussianPostAvaible]);
 
   const onFinish = (values: any) => {
 
